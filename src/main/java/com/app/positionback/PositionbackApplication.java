799e566b81bb02ec7f57c53c3e8d45666cffc1ac
kimgame102 <koreait2337@gmail.com>
2024-10-21 12:34:38 +0900

    init

---
+++ src/main/java/com/app/positionback/PositionbackApplication.java
@@ -0,0 +1,13 @@
+package com.app.positionback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PositionbackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PositionbackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/app/positionback/PositionbackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.positionback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PositionbackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
