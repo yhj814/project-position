038bf36dac71aa1d3105f3e130991bb6a7e2c2a8
hamandy2000 <minji9306@gmail.com>
2024-10-24 13:56:55 +0900

    memberVO,DTO init

--- src/main/java/com/app/positionback/domain/member/MemberDTO.java
+++ src/main/java/com/app/positionback/domain/member/MemberDTO.java
@@ -1,13 +1,15 @@
 package com.app.positionback.domain.member;
 
-import lombok.*;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 import org.springframework.stereotype.Component;
 
 @Component
-@Getter @ToString @EqualsAndHashCode(onlyExplicitlyIncluded = true)
-@NoArgsConstructor
-@AllArgsConstructor
-public class MemberVO {
+@Getter @Setter @ToString
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class MemberDTO {
     @EqualsAndHashCode.Include
     private Long id;
     private String memberName;
@@ -19,4 +21,6 @@ public class MemberVO {
     private String memberStatus;
     private String memberType;
     private String memberWarningCount;
+
+    public MemberVO toVo(){return new MemberVO(id,memberName,memberEmail,memberPassword,memberAddress,memberAddressDetail,memberNickname,memberStatus,memberType,memberWarningCount);}
 }
