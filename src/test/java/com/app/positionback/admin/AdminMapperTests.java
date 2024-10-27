b78ace7806c7dc926e870a799174a3903af62b42
kimgame102 <koreait2337@gmail.com>
2024-10-28 08:55:37 +0900

    init

--- src/test/java/com/app/positionback/admin/AdminMapperTests.java
+++ src/test/java/com/app/positionback/admin/AdminMapperTests.java
@@ -1,4 +1,19 @@
 package com.app.positionback.admin;
 
+import com.app.positionback.mapper.admin.AdminMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@Slf4j
 public class AdminMapperTests {
+    @Autowired
+    private AdminMapper adminMapper;
+
+    @Test
+    public void testAdminMapper() {
+
+    }
 }
