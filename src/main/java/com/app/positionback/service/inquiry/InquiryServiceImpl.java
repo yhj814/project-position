98543080b2275fc8c16b20b65375b4904331d5de
kimgame102 <koreait2337@gmail.com>
2024-10-28 09:12:15 +0900

    init

--- src/main/java/com/app/positionback/service/inquiry/InquiryServiceImpl.java
+++ src/main/java/com/app/positionback/service/inquiry/InquiryServiceImpl.java
@@ -1,6 +1,9 @@
 package com.app.positionback.service.inquiry;
 
+import com.app.positionback.domain.inquiry.InquiryDTO;
 import com.app.positionback.domain.inquiry.InquiryVO;
+import com.app.positionback.domain.member.MemberVO;
+import com.app.positionback.domain.corporation.CorporationVO;
 import com.app.positionback.repository.inquiry.InquiryDAO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -12,9 +15,19 @@ import org.springframework.transaction.annotation.Transactional;
 public class InquiryServiceImpl implements InquiryService {
     private final InquiryDAO inquiryDAO;
 
+    public void processInquiry(InquiryDTO inquiryDTO) {
+        if ("기업".equals(inquiryDTO.getInquiryType())) {
+            CorporationVO corpVO = inquiryDTO.toCorpVO();
+            inquiryDAO.insertForCorporation(corpVO); // DAO 호출
+        } else {
+            MemberVO memberVO = inquiryDTO.toMemVO();
+            inquiryDAO.insertForMember(memberVO); // DAO 호출
+        }
+    }
+
     @Override
     public void write(InquiryVO inquiryVO){
         inquiryDAO.insert(inquiryVO);
     };
-
 }
+
