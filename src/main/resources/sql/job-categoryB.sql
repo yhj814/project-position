a661d5b619329785696a64543a88f20b332acce9
Seok-SangHun <amen101673@gmail.com>
2024-10-29 16:13:05 +0900

    1029 mid finish

--- src/main/resources/sql/job-categoryB.sql
+++ src/main/resources/sql/job-categoryB.sql
@@ -8,7 +8,11 @@ create table tbl_job_categoryB(
 
 select * from tbl_job_categoryB;
 
--- 직무·직업
-INSERT INTO tbl_job_categoryB (job_categoryB_name, job_categoryA_id) VALUES
-                                                                         ('직무·직업', 2),
-                                                                         ('전문 분야', 2);
+-- 직무·직업 (ID = 20)
+INSERT INTO tbl_job_categoryB (job_categoryB_name, job_categoryA_id)
+VALUES ('직무·직업', 9),
+       ('담당분야', 9);  -- '고객상담·TM'의 ID인 9로 설정
+
+-- 담당분야 (ID = 21)
+INSERT INTO tbl_job_categoryB (job_categoryB_name, job_categoryA_id)
+VALUES ('담당분야', 9);  -- '고객상담·TM'의 ID인 9로 설정
