15f022e391f17a514aa2a4432a0d9b5bdb54a8d7
kimgame102 <koreait2337@gmail.com>
2024-10-30 14:08:30 +0900

    init

--- src/main/java/com/app/positionback/mapper/inquiry/InquiryMapper.java
+++ src/main/java/com/app/positionback/mapper/inquiry/InquiryMapper.java
@@ -1,6 +1,5 @@
 package com.app.positionback.mapper.inquiry;
 
-import com.app.positionback.domain.inquiry.InquiryDTO;
 import com.app.positionback.domain.inquiry.InquiryVO;
 import org.apache.ibatis.annotations.Mapper;
 
@@ -8,8 +7,5 @@ import org.apache.ibatis.annotations.Mapper;
 public interface InquiryMapper {
 
     // 1:1 문의 작성
-    public void insertInquiry(InquiryDTO inquiryDTO);
-
-    // 가장 최근 추가된 ID 가져오기
-    public Long getRecentInsertId();
+    public void insert(InquiryVO inquiryVO);
 }
--- src/main/java/com/app/positionback/service/inquiry/InquiryService.java
+++ src/main/java/com/app/positionback/service/inquiry/InquiryService.java
@@ -1,9 +1,7 @@
 package com.app.positionback.service.inquiry;
 
-import com.app.positionback.domain.inquiry.InquiryDTO;
 import com.app.positionback.domain.inquiry.InquiryVO;
-import org.springframework.web.multipart.MultipartFile;
 
 public interface InquiryService {
-    public void saveInquiry(InquiryDTO inquiryDTO, MultipartFile file) throws Exception;
+    public void write(InquiryVO inquiryVO);
 }
